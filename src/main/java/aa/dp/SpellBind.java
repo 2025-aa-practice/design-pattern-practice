238dcb6c3be35d0673cedecd2cafbd490f832d46
Gomm <ckdehd200@gmail.com>
2025-07-18 14:46:44 +0900

    fix: change message to english

--- src/main/java/aa/dp/SpellBind.java
+++ src/main/java/aa/dp/SpellBind.java
@@ -3,25 +3,25 @@ package aa.dp;
 public class SpellBind implements SpellShootable {
     @Override
     public void spell(WolfGroup target) {
-        System.out.println("늑대 무리에게 발묶기 마법을 사용합니다.");
+        System.out.println("Cast bind to wolf group.");
         for(IWolf wolf:target.getWolfs()) {wolf.hitMagic(this);}
     }
 
     @Override
     public void spell(Wolf target) {
-        System.out.println("늑대의 발이 묶였습니다.");
+        System.out.println("The wolf is binded.");
     }
 
     @Override
     public void spell(Giant target) {
-        System.out.println("거인에게 발묶기 마법을 사용합니다.");
-        System.out.println("거인의 발이 묶였습니다.");
+        System.out.println("Cast bind to Giant.");
+        System.out.println("The giant is binded");
     }
 
     @Override
     public void spell(Dragon target) {
-        System.out.println("용에게 발묶기 마법을 사용합니다.");
-        System.out.println("하늘을 나는 용은 발이 묶이지 않습니다..");
+        System.out.println("Cast bind to Giant.");
+        System.out.println("The dragon cannot be binded.");
     }
 
 }
--- src/main/java/aa/dp/SpellBlind.java
+++ src/main/java/aa/dp/SpellBlind.java
@@ -3,24 +3,24 @@ package aa.dp;
 public class SpellBlind implements SpellShootable {
     @Override
     public void spell(WolfGroup target) {
-        System.out.println("늑대 무리에게 눈멀기 마법을 사용합니다.");
+        System.out.println("Cast blind to wolf group.");
         for(IWolf wolf:target.getWolfs()) {wolf.hitMagic(this);}
     }
     @Override
     public void spell(Wolf target) {
-        System.out.println("늑대의 눈이 멀었지만 냄새를 맡습니다.");
+        System.out.println("The wolf is blinded, but it can still smell.");
     }
 
     @Override
     public void spell(Giant target) {
-        System.out.println("거인에게 눈멀기 마법을 사용합니다.");
-        System.out.println("거인은 눈이 멀었습니다.");
+        System.out.println("Cast blind to Giant.");
+        System.out.println("The giant is blind.");
     }
 
     @Override
     public void spell(Dragon target) {
-        System.out.println("용에게 눈멀기 마법을 사용합니다.");
-        System.out.println("용은 눈이 멀었습니다.");
+        System.out.println("Cast blind to dragon.");
+        System.out.println("The dragon is blind.");
     }
 
 }
